c1c7161f76860e76405d01f3b807f167b5eff518
taf1kkk <taf1kcrypto@gmail.com>
2022-09-21 15:21:47 +0300

    complete all

--- 1/first calc/Calc.js
+++ 1/first calc/Calc.js
@@ -1,19 +1,15 @@
-var rootA = Number(document.querySelector("#inputA").value);
-var rootB = Number(document.querySelector("#inputB").value);
-var rootC = Number(document.querySelector("#inputC").value);
-
 function result() {
     var rootA = Number(document.querySelector("#inputA").value);
     var rootB = Number(document.querySelector("#inputB").value);
     var rootC = Number(document.querySelector("#inputC").value);
     let D = 0;
 
-    if (rootA == 0 || rootA < 0) {
+    if (rootA == 0) {
         rootA = 1;
-        return D = (rootB * rootB) - (4 * (rootA * rootC));
+        return D = Math.pow(rootB, 2) - (4 * rootA * rootC);
     }
     else {
-        return D = (rootB * rootB) - (4 * (rootA * rootC));
+        return D = Math.pow(rootB, 2) - (4 * rootA * rootC);
     }
 
 }
@@ -21,22 +17,26 @@ function result() {
 function check() {
     var rootA = Number(document.querySelector("#inputA").value);
     var rootB = Number(document.querySelector("#inputB").value);
-    if(rootA == 0 || rootA < 0){
+    if(rootA == 0){
         rootA = 1;
     }
-    let x1 = 0;
-    let x2 = 0;
+
+    let Value1 = 0;
+    let Value2 = 0;
     if (result() < 0) {
         document.querySelector("#firstR").innerHTML = "Корней нет";
 
     }
     else if (result() === 0) {
-        x1 = (-rootB + Math.sqrt(result())) / (2 * rootA);
+        Value1 = (-rootB + Math.sqrt(result())) / (2 * rootA);
+        let x1 = Math.round(Value1)
         document.querySelector("#firstR").innerHTML = "X1 = " + x1;
     }
     else if (result() > 0) {
-        x1 = (-rootB - Math.sqrt(result())) / 2 * rootA;
-        x2 = (-rootB + Math.sqrt(result())) / 2 * rootA;
+        Value1 = (-rootB - Math.sqrt(result())) / 2 * rootA;
+        Value2 = (-rootB + Math.sqrt(result())) / 2 * rootA;
+        let x1 = Math.round(Value1)
+        let x2 = Math.round(Value2)
         document.querySelector("#firstR").innerHTML = "X1 = " + x1;
         document.querySelector("#secondR").innerHTML = "X2 = " + x2;
     }
